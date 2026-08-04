398ad95a2644b3faf47fce9b8dffd460e046820e
NhorSengAn <nhor.sengan.168@gmail.com>
2026-08-05 02:16:13 +0700

    import product from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,10 @@
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+
+
 
 let productsHTML = "";
+
 products.forEach((product) => {
   productsHTML += `
          <div class="product-container">
