31bb08c864d99f5e5a4ba91a3d601d68d6b2d272
NhorSengAn <nhor.sengan.168@gmail.com>
2026-08-09 03:27:23 +0700

    deliveryOption date

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOption = [
+  {
+    id: "1",
+    deliverDays: 7,
+    priceCents: 0,
+  },
+  {
+    id: "2",
+    deliverDays: 2,
+    priceCents: 499,
+  },
+  {
+    id: "3",
+    deliverDays: 3,
+    priceCents: 999,
+  },
+];
