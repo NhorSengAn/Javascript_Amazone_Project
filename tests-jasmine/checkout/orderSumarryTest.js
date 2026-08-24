b67128ad2d219247a0df8aab9cc00f9e4ff460af
NhorSengAn <nhor.sengan.168@gmail.com>
2026-08-24 18:11:25 +0700

    Complete the hooks

--- tests-jasmine/checkout/orderSumarryTest.js
+++ tests-jasmine/checkout/orderSumarryTest.js
@@ -2,14 +2,15 @@ import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
 
 describe("Test suite: renderOrderSummary", () => {
-  it("displays the cart", () => {
+  const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+  const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
+  beforeEach(() => {
     spyOn(localStorage, "setItem");
     document.querySelector(".js-test-container2").innerHTML = `
         <div class="js-order-summary"></div>
+        <div class="js-checkout-header"></div>
+        <div class="js-payment-summary"></div>
     `;
-
-    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 
     spyOn(localStorage, "getItem").and.callFake(() => {
       return JSON.stringify([
@@ -28,7 +29,8 @@ describe("Test suite: renderOrderSummary", () => {
 
     loadFromStorage();
     renderOrderSummary();
-
+  });
+  it("displays the cart", () => {
     expect(document.querySelectorAll(".cart-item-container").length).toEqual(2);
 
     expect(
@@ -42,34 +44,6 @@ describe("Test suite: renderOrderSummary", () => {
   });
 
   it("remove a product", () => {
-    spyOn(localStorage, "setItem");
-    document.querySelector(".js-test-container2").innerHTML = `
-        <div class="js-order-summary"></div>
-        <div class="js-checkout-header"></div>
-        <div class="js-payment-summary"></div>
-    `;
-
-    const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
-    const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
-    spyOn(localStorage, "getItem").and.callFake(() => {
-      return JSON.stringify([
-        {
-          productId: productId1,
-          quantity: 2,
-          deliveryOptionId: "1",
-        },
-        {
-          productId: productId2,
-          quantity: 1,
-          deliveryOptionId: "2",
-        },
-      ]);
-    });
-
-    loadFromStorage();
-
-    renderOrderSummary();
-
     document.querySelectorAll(".js-delete-link").forEach((link) => {
       if (link.dataset.productId === productId1) {
         link.click();
