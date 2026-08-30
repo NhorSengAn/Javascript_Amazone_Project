ae4577fead0776f0c216c687bd16ae5152a7966a
NhorSengAn <nhor.sengan.168@gmail.com>
2026-08-30 23:00:00 +0700

    convert XHRResquest to Fecth promise

--- data/products.js
+++ data/products.js
@@ -81,8 +81,32 @@ export class Appliance extends Product {
     `;
   }
 }
-export let products = [];
+export function loadProductsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((productsData) => {
+      products = productsData.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+          return new Clothing(productDetails);
+        } else if (productDetails.type === "appliance") {
+          return new Appliance(productDetails);
+        }
+        return new Product(productDetails);
+      });
 
+      console.log("load Product");
+    });
+
+  return promise;
+}
+
+/*
+loadProductsFetch();
+*/
+
+export let products = [];
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -621,8 +645,6 @@ export const products = [
 */
 
 /*
-
-
 const date = new Date();
  console.log(date);
  console.log(date.toLocaleDateString());
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,13 +1,14 @@
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 // import "../data/cart-class.js";
 // import "../data/car.js";
 // import "../data/backend-practies.js";
 import { loadCarts } from "../data/cart.js";
 
 Promise.all([
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadProducts(() => {
       resolve("value1"); // we can use resolve to wait after finish the next step
